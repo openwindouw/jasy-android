d5e487ab3d4e369950ed594e826efb845bb23617
Vladimir Espinola <vladimir.espinola.contractor@bbva.com>
2019-09-24 18:47:58 -0400

    added mdc

--- app/src/main/java/com/example/jasy/Helpers/Constants/NASAConstants.kt
+++ app/src/main/java/com/example/jasy/Helpers/Constants/NASAConstants.kt
@@ -4,5 +4,6 @@ class NASAConstants {
     companion object{
         val apiKey = "j3QsWa596qx2WxAMvZtxJAM4oH55JiV9mbIxO2Ng"
         val baseURL = "https://api.nasa.gov/"
+        val minimumStartingPoint = "1995-06-16"
     }
 }
